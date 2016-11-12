d940606b45545a5068c3e782d2aabaa6287a8411
Pumpe <zzplxm1988@gmail.com>
2016-11-12 19:33:40 +0800

    增加成功页面，个人中心页面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,3 +1,18 @@
 Page({
-    data:{}
+    data:{},
+    onLoad:function(){
+        var me = this;
+        wx.getUserInfo({
+            success: function(res) {
+                var userInfo = res.userInfo;
+                me.setData({
+                    userInfo:{
+                        avatar:userInfo.avatarUrl,
+                        name:userInfo.nickName
+                    }
+                })
+            },
+            fail:function(err){}
+        })
+    }
 })
