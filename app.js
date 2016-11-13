af8ae402510656795b66dc84484d02251fea39cb
Pumpe <zzplxm1988@gmail.com>
2016-11-13 13:07:46 +0800

    修改更新列表页

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 App({
+    uid:0,
     onLaunch:function(){
-        wx.checkSession({
-            fail: function(){
+        // wx.checkSession({
+        //     fail: function(){
+                var that = this;
                 wx.login({
                     success: function(res) {
                         if (res.code) {
                             //发起网络请求
                             wx.request({
-                                url: 'https://2bsapi.360che.com/wxadoc/index.php',
+                                url: 'https://2bsapi.360che.com/56/',
                                 data: {
+                                    c:'wxadoc',
                                     code: res.code
+                                },
+                                success:function(res){
+                                    if(res && res.data){
+                                        that.uid = res.data['data'].uid
+                                    }
                                 }
                             })
                         } else {
@@ -17,7 +25,7 @@ App({
                         }
                     }
                 });
-            }
-        });
+        //     }
+        // });
     }
 });
